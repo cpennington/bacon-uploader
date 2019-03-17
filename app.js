1939cf7bc34b79c75a47c3ec1f4a9c0dbcd995cf
Calen Pennington <cale@edx.org>
2019-03-17 00:22:20 -0400

    Upload to base point of bucket

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var albumBucketName = 'bacon-uploader';
-var bucketRegion = 'us-east-1';
-var IdentityPoolId = 'us-east-1:a7daa586-be94-47b4-bbf0-9f2a67bb1c41';
+var bucketName = "bacon-uploader";
+var bucketRegion = "us-east-1";
+var IdentityPoolId = "us-east-1:a7daa586-be94-47b4-bbf0-9f2a67bb1c41";
 
 AWS.config.update({
   region: bucketRegion,
@@ -10,34 +10,36 @@ AWS.config.update({
 });
 
 var s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  params: {Bucket: albumBucketName}
+  apiVersion: "2006-03-01",
+  params: { Bucket: bucketName }
 });
 
 function showUploader() {
   var htmlTemplate = [
     '<input id="replayupload" type="file" accept=".bcr">',
     '<button id="uploadreplay" onclick="uploadReplays()">',
-      'Upload Replays',
-    '</button>',
+    "Upload Replays",
+    "</button>"
   ];
-  document.getElementById('app').innerHTML = getHtml(htmlTemplate);
+  document.getElementById("app").innerHTML = getHtml(htmlTemplate);
 }
 
 function uploadReplays() {
-  var files = document.getElementById('replayupload').files;
+  var files = document.getElementById("replayupload").files;
   var file = files[0];
   var fileName = file.name;
-  var bucketKey = encodeURIComponent("bacon-uploader") + '//';
 
-  var objectKey = bucketKey + fileName;
-  s3.upload({
-    Key: objectKey,
-    Body: file,
-  }, function(err, data) {
-    if (err) {
-      return alert('There was an error uploading your replay: ', err.message);
+  var objectKey = fileName;
+  s3.upload(
+    {
+      Key: objectKey,
+      Body: file
+    },
+    function(err, data) {
+      if (err) {
+        return alert("There was an error uploading your replay: ", err.message);
+      }
+      alert("Successfully uploaded replays.");
     }
-    alert('Successfully uploaded replays.');
-  });
+  );
 }
