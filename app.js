7f7ab8689120cb440ad9733da04076b8693c5007
Calen Pennington <cale@edx.org>
2019-03-19 10:43:45 -0400

    Convert to requesting zip files

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var s3 = new AWS.S3({
 
 function showUploader() {
   var htmlTemplate = [
-    '<input id="replayupload" type="file" accept=".bcr" multiple>',
+    '<input id="replayupload" type="file" accept=".zip" multiple>',
     '<button id="uploadreplay" onclick="uploadReplays()">',
     "Upload Replays",
     "</button>"
